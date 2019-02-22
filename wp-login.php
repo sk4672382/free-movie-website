0c61f010c4fa8d1413d93dadfdd4992c6f00657e
sk4672382 <47851801+sk4672382@users.noreply.github.com>
2019-02-22 16:41:21 +0530

    final

--- wp-login.php
+++ wp-login.php
@@ -9,56 +9,57 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title>Log In &lsaquo; Movies &#8212; WordPress</title>
 	<link rel='dns-prefetch' href='//s.w.org' />
-<link rel='stylesheet' id='dashicons-css'  href='https://sk4672382.github.io/free-movie-website/wp-includes/css/dashicons.min.css?ver=5.0.3' type='text/css' media='all' />
-<link rel='stylesheet' id='buttons-css'  href='https://sk4672382.github.io/free-movie-website/wp-includes/css/buttons.min.css?ver=5.0.3' type='text/css' media='all' />
-<link rel='stylesheet' id='forms-css'  href='https://sk4672382.github.io/free-movie-website/wp-admin/css/forms.min.css?ver=5.0.3' type='text/css' media='all' />
-<link rel='stylesheet' id='l10n-css'  href='https://sk4672382.github.io/free-movie-website/wp-admin/css/l10n.min.css?ver=5.0.3' type='text/css' media='all' />
-<link rel='stylesheet' id='login-css'  href='https://sk4672382.github.io/free-movie-website/wp-admin/css/login.min.css?ver=5.0.3' type='text/css' media='all' />
+<link rel='stylesheet' id='dashicons-css'  href='https://sk4672382.github.io/free-movie-website/wp-includes/css/dashicons.min.css?ver=5.1' type='text/css' media='all' />
+<link rel='stylesheet' id='buttons-css'  href='https://sk4672382.github.io/free-movie-website/wp-includes/css/buttons.min.css?ver=5.1' type='text/css' media='all' />
+<link rel='stylesheet' id='forms-css'  href='https://sk4672382.github.io/free-movie-website/wp-admin/css/forms.min.css?ver=5.1' type='text/css' media='all' />
+<link rel='stylesheet' id='l10n-css'  href='https://sk4672382.github.io/free-movie-website/wp-admin/css/l10n.min.css?ver=5.1' type='text/css' media='all' />
+<link rel='stylesheet' id='login-css'  href='https://sk4672382.github.io/free-movie-website/wp-admin/css/login.min.css?ver=5.1' type='text/css' media='all' />
 	<meta name='robots' content='noindex,noarchive' />
 	<meta name='referrer' content='strict-origin-when-cross-origin' />
 		<meta name="viewport" content="width=device-width" />
 		</head>
 	<body class="login login-action-login wp-core-ui  locale-en-us">
 		<div id="login">
-		<h1><a href="https://wordpress.org/" title="Powered by WordPress" tabindex="-1">Powered by WordPress</a></h1>
+		<h1><a href="https://wordpress.org/" title="Powered by WordPress">Powered by WordPress</a></h1>
 	
-<form name="loginform" id="loginform" action="https://sk4672382.github.io/free-movie-website/wp-login.php" method="post">
+	<form name="loginform" id="loginform" action="https://sk4672382.github.io/free-movie-website/wp-login.php" method="post">
 	<p>
 		<label for="user_login">Username or Email Address<br />
-		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
+		<input type="text" name="log" id="user_login" class="input" value="" size="20" autocapitalize="off" /></label>
 	</p>
 	<p>
 		<label for="user_pass">Password<br />
 		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
 	</p>
-		<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Remember Me</label></p>
+			<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Remember Me</label></p>
 	<p class="submit">
 		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
-		<input type="hidden" name="redirect_to" value="https://sk4672382.github.io/free-movie-website/wp-admin/" />
-		<input type="hidden" name="testcookie" value="1" />
+				<input type="hidden" name="redirect_to" value="https://sk4672382.github.io/free-movie-website/wp-admin/" />
+					<input type="hidden" name="testcookie" value="1" />
 	</p>
-</form>
+	</form>
 
-<p id="nav">
-	<a href="https://sk4672382.github.io/free-movie-website/wp-login.php?action=lostpassword">Lost your password?</a>
-</p>
+			<p id="nav">
+					<a href="https://sk4672382.github.io/free-movie-website/wp-login.php?action=lostpassword">Lost your password?</a>
+				</p>
+	
+	<script type="text/javascript">
+	function wp_attempt_focus(){
+	setTimeout( function(){ try{
+			d = document.getElementById('user_login');
+				d.focus();
+	d.select();
+	} catch(e){}
+	}, 200);
+	}
 
-<script type="text/javascript">
-function wp_attempt_focus(){
-setTimeout( function(){ try{
-d = document.getElementById('user_login');
-d.focus();
-d.select();
-} catch(e){}
-}, 200);
-}
+			wp_attempt_focus();
+			if(typeof wpOnload=='function')wpOnload();
+			</script>
 
-wp_attempt_focus();
-if(typeof wpOnload=='function')wpOnload();
-</script>
-
-	<p id="backtoblog"><a href="https://sk4672382.github.io/free-movie-website/">&larr; Back to Movies</a></p>
-		
+			<p id="backtoblog"><a href="https://sk4672382.github.io/free-movie-website/">
+		&larr; Back to Movies	</a></p>
+			
 	</div>
 
 	
